e0677de2bc265edfec01d1d268c43d22054ca066
Iván Sánchez <ivansancrodr@gmail.com>
2020-09-08 00:40:07 +0200

    get todas tablas funcional, añadido regex para mejorar que la busqueda funcione con partes de una palabra e ignore mayúsculas

--- controllers/searchs.js
+++ controllers/searchs.js
@@ -2,14 +2,28 @@
 
 const { response } = require("express");
 
+const User = require('../models/user');
+const Doctor = require('../models/doctor');
+const Hospital = require('../models/hospital');
+
 const getGeneral = async(req, res = response) => {
 
-    const query = req.params.searchs
+    const query = req.params.searchs;
+    const regex = new RegExp( query, 'i')
     
-    res.json({
+    
+
+    const [users, doctors, hospitals] = await Promise.all([
+         User.find({ name: regex }),
+         Doctor.find({ name: regex }),
+         Hospital.find({ name: regex })
+    ])
+    
+        res.json({
         ok:true,
-        msg: 'getGeneral',
-        query
+        users,
+        doctors,
+        hospitals
     })
 
 }
